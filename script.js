2475411a83f4070abe83a453f62be1c14fc653c2
Michaela Brodová <mi.brodova@seznam.cz>
2023-10-27 23:13:38 +0200

    změny

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Firma nabízející trička s potiskem poskytuje množstevní slevy podle počtu objednaný kusů. Při objednávce pod 50 kusů stojí jedno tričko 300 Kč. Pokud si objednáme alespoň 50 kusů, je cena 250 Kč za kus. Při objednávce nad 200 kusů je cena 200 Kč za tričko. Nad 500 kusů zaplatíme 150 Kč za tričko a nad 1000 kusů zaplatíme 120 korun za tričko.
+
+Napište stránku, která od uživatele obdrží počet kusů, které si chce objednat, a program odpoví celkovou ceny objednávky výpisem do stránky.
+*/
+
+const kusy = Number(prompt("Zadejte počet kusů"))
+
+
+if(kusy < 50) {
+    const celkovaCena = kusy * 300
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena} </p>`
+} else if(kusy >= 50 && kusy < 200) {
+     const celkovaCena2 = kusy * 250
+     document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena2} </p>`
+} else if(kusy >= 200 && kusy < 500) {
+    const celkovaCena3 = kusy * 200
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena3} </p>`
+} else if(kusy >= 500 && kusy < 1000) {
+    const celkovaCena4 = kusy * 150
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena4} </p>` 
+} else if(kusy >= 1000) {
+    const celkovaCena5 = kusy * 120
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena5} </p>`
+} 
