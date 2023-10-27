704dadc6c42236c8471577345f29fbff66841571
Michaela Brodová <mi.brodova@seznam.cz>
2023-10-27 23:17:13 +0200

    změna

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ const kusy = Number(prompt("Zadejte počet kusů"))
 
 if(kusy < 50) {
     const celkovaCena = kusy * 300
-    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena} </p>`
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena}Kč </p>`
 } else if(kusy >= 50 && kusy < 200) {
      const celkovaCena2 = kusy * 250
-     document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena2} </p>`
+     document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena2}Kč </p>`
 } else if(kusy >= 200 && kusy < 500) {
     const celkovaCena3 = kusy * 200
-    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena3} </p>`
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena3}Kč </p>`
 } else if(kusy >= 500 && kusy < 1000) {
     const celkovaCena4 = kusy * 150
-    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena4} </p>` 
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena4}Kč </p>` 
 } else if(kusy >= 1000) {
     const celkovaCena5 = kusy * 120
-    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena5} </p>`
+    document.body.innerHTML = `<p> Konečná cena je: ${celkovaCena5}Kč </p>`
 } 
